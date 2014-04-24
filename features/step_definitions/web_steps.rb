14795abf46b61c8db12819ace390116a0435deea
lamine kaba <lkaba2003@yahoo.com>
2014-04-23 20:08:23 -0500

    Creation of projects feature complete

---
+++ features/step_definitions/web_steps.rb
@@ -0,0 +1,21 @@
+
+
+Given(/^(?:|I )am on (.+)$/) do |page_name|
+ 
+end
+
+When(/^I follow "(.*?)"$/) do |arg1|
+  
+end
+
+When(/^I fill in "(.*?)" with "(.*?)"$/) do |arg1, arg2|
+  
+end
+
+When(/^I press "(.*?)"$/) do |arg1|
+  
+end
+
+Then(/^I should see "(.*?)"$/) do |arg1|
+  
+end
